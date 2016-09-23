94f08c84431fa7def8f1d3558e4bc21f98daf1ad
Jake Hamill <hamilljake1@gmail.com>
2016-09-23 14:39:10 -0500

    NW

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
+var t = 0;
+var r = 0;
+var s = 0
+var rr = 0;
+
+
 function setup() {
-  createCanvas(400,250);
+  createCanvas(900,700);
+  background(255);
 }
 
 function draw() {
-  ///translate(mouseX, mouseY);
-  rect(0,0,30,30);
+e = random(120, 200);
+g = random(120, 200);
+b = random(120, 200);
+
+translate(width/2, height/2);
+
+translate(t, r)
+ t = t + 1;
+rotate(rr);
+ rr = rr + PI;
+rotate(r);
+ r = r + 2;
+fill(e , g, b);
+ellipse(10, 10, 100, 30);
+
 }
